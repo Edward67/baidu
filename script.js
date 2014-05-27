fd9bf79a8a9f5029c73507baad4b7d33335f0a20
Edward67 <edward67@foxmail.com>
2014-05-27 13:38:32 +0800

    Revert "添加了请求文字"
    This reverts commit 5bdfa4a643b9765f0e644c4b9ecf73a15a3bc4ae.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+(function(){
 	"use strict";
 	
 	var c = null,
@@ -6,39 +6,13 @@ window.onload = function(){
 		canvasWidth = null,
 		canvasHeight = null,
 		point = [], // 储存点的数组
-		wordList = [], // 储存文字的数组
 		pNum = 0, // 点的序号
+		word = "", // 显示的文字
+		fontSize = 14, //字的大小
+		pointNums = 30, // 点的数量
 		mouseX = 0,
 		mouseY = 0,
-		i = 0,
-		wordObj = document.getElementsByTagName("div")[0];
-	
-	Number.prototype.toColor = function(opacity){
-	
-		// 这里可能会有 opacity 为 0 触发错误的问题 , 
-		opacity = opacity || 1;
-		return "rgba(" + this + ", " + this + ", " + this + ", " + opacity + ")";
-	}
-	
-	// 我尝试了script用src发jsonp，发现目标页面不支持，于是我只能本地服务器php抓取后交给前端处理了
-	var xmlhttp = null;	
-	if (window.XMLHttpRequest)
-		xmlhttp = new XMLHttpRequest();
-	else if (window.ActiveXObject)
-		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	
-	if (xmlhttp !== null){
-		xmlhttp.open("GET", "getWord.php", true);
-		xmlhttp.send();
-		xmlhttp.onreadystatechange = function(){
-			if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-			
-				wordList = JSON.parse(xmlhttp.responseText);
-				
-				init();
-			}
-		};
-	}
+		i = 0;
 	
 	function init(){
 		c = document.createElement("canvas");
@@ -52,25 +26,22 @@ window.onload = function(){
 		c.height = canvasHeight;   
 		document.body.appendChild(c);
 		
-		// 根据 wordList 的个数 创建相应点的数量
-		for (i = 0; i < wordList.length; i++){
+		// 创建 pointNums 个点
+		for (i = 0; i < pointNums; i++){
 			point.push(createPoint());
 		}
 		
 		// 初始化点
 		function createPoint(){
 			var p = {
-				num : pNum,
+				num : pNum++,
 				x : Math.random() * canvasWidth,
 				y : Math.random() * canvasHeight,
 				size : Math.random() + 1, // 点的大小
 				a : Math.random() * 2 * Math.PI, // 初始角度
 				speed : Math.random() * 0.6, // 速度
-				word : wordList[pNum++],
-				isStop : false, // 是否停止
-				distTo : function(p){ // 判断当前点与参数中的点的距离
-					return Math.sqrt((this.x - p.x) * (this.x - p.x) + (this.y - p.y) * (this.y - p.y));
-				}
+				word : "这是第 " + (pNum - 1) + " 个点",
+				isStop : false // 是否停止
 			};
 			return p;
 		}
@@ -84,10 +55,7 @@ window.onload = function(){
 			point.forEach(function(p){
 				if ( Math.abs(p.x  - mouseX) < 20 && Math.abs(p.y - mouseY) < 20){
 					p.isStop = true;
-					wordObj.innerHTML = p.word;
-					wordObj.style.left = (mouseX + wordObj.offsetWidth < canvasWidth ? mouseX + 20 : mouseX - wordObj.offsetWidth) + "px";
-					wordObj.style.top = (mouseY + wordObj.offsetHeight < canvasHeight ? mouseY + 20 : mouseY - wordObj.offsetHeight) + "px";
-					wordObj.style.visibility = "visible";
+					word = p.word;
 				}else{
 					p.isStop = false;
 					nums++;
@@ -96,12 +64,10 @@ window.onload = function(){
 			});
 			
 			if (nums == point.length){
-				wordObj.style.visibility = "hidden";
+				word = "";
 			}
 		
 		}, true);
-		
-		draw();
 	}
 	
 	window.onresize = function () {
@@ -110,20 +76,108 @@ window.onload = function(){
 		}
 		point.length = 0;
 		pNum = 0;
+		word = "";
 		mouseX = 0;
 		mouseY = 0;
 		
-		// 防止数据请求之前出现画面
-		if (wordList.length > 0){
-			init();
-		}
+		init();
 	};
 	
+	init();
 	
 	function draw(){
-		
 		var color = 190; // 初始颜色
 		var distList = []; // 储存所有的距离
+		
+		ctx.lineWidth = "1";
+		
+		// 计算距离
+		for (var i = 0, len = point.length; i < len; i++){
+			for (var j = 0; j < i; j++){
+				distList.push({
+							dist: getDist(point[i], point[j]),
+							x1: point[i].x,
+							y1: point[i].y,
+							x2: point[j].x,
+							y2: point[j].y});
+			}
+		}
+		
+		// 从大到小排序
+		distList.sort(function(a, b){
+			return b.dist - a.dist;
+		});
+		
+		
+		// 画连接线
+		distList.forEach(function(p){
+			if (color + p.dist / 3 < 256){
+				ctx.beginPath();
+				ctx.strokeStyle = getColor(parseInt(color + p.dist / 3));
+				ctx.moveTo(p.x1, p.y1);
+				ctx.lineTo(p.x2, p.y2);
+				ctx.stroke();
+			}
+		});
+		
+		// 画点的边框
+		point.forEach(function(p){
+			ctx.beginPath();
+			
+			// 判断当前点是否是停留状态
+			if (p.isStop){
+				ctx.strokeStyle = getColor(color, 0.2);
+				ctx.fillStyle = getColor(color, 0.3);
+				ctx.arc(p.x, p.y, p.size * 4, 0, 2 * Math.PI);
+			}else{
+				ctx.strokeStyle = getColor(color, 0.1);
+				ctx.fillStyle = getColor(color, 0.15);
+				ctx.arc(p.x, p.y, p.size * 3, 0, 2 * Math.PI);
+			}
+			
+			ctx.fill();
+			ctx.stroke();
+		});
+		
+		// 画点
+		ctx.strokeStyle = getColor(color, 0.5);
+		ctx.fillStyle = getColor(color, 0.8);
+		point.forEach(function(p){
+			ctx.beginPath();
+			ctx.arc(p.x, p.y, p.size, 0, 2 * Math.PI);
+			ctx.fill();
+			ctx.stroke();
+		});
+		
+		// 如果当前存在文字状态 即鼠标停留附近有点
+		if (word !== ""){
+			ctx.font = fontSize + "px 微软雅黑";
+			ctx.fillStyle = getColor(140);
+			
+			if (word.length * fontSize + 20 + mouseX > canvasWidth){
+				ctx.fillText(word, mouseX - word.length * fontSize, mouseY + 20);
+			}else{
+				ctx.fillText(word, mouseX + 20, mouseY + 20);
+			}
+			
+		}
+		
+		distList.length = 0;
+	}
+	
+	
+	function getColor(color, opacity){
+	
+		// 这里可能会有 opacity 为 0 触发错误的问题 , 
+		opacity = opacity || 1;
+		return "rgba(" + color + ", " + color + ", " + color + ", " + opacity + ")";
+	}
+	
+	function getDist(p1, p2){
+		return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+	}
+	
+	var timer = setInterval(function(){
 		
 		// 计算新的点的位置
 		point.forEach(function(p){
@@ -146,69 +200,11 @@ window.onload = function(){
 			}
 		});
 		
-		// 计算距离
-		for (var i = 0, len = point.length; i < len; i++){
-			for (var j = 0; j < i; j++){
-				distList.push({
-							dist: point[i].distTo(point[j]),
-							x1: point[i].x,
-							y1: point[i].y,
-							x2: point[j].x,
-							y2: point[j].y});
-			}
-		}
-		
-		// 从大到小排序
-		distList.sort(function(a, b){
-			return b.dist - a.dist;
-		});
-		
-		// 清除画布
 		ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+		draw();
 		
-		// 画连接线
-		distList.forEach(function(p){
-			if (color + p.dist / 3 < 256){
-				ctx.beginPath();
-				ctx.strokeStyle = parseInt(color + p.dist / 3).toColor();
-				ctx.moveTo(p.x1, p.y1);
-				ctx.lineTo(p.x2, p.y2);
-				ctx.stroke();
-			}
-		});
-		
-		// 画点的边框
-		point.forEach(function(p){
-			ctx.beginPath();
-			
-			// 判断当前点是否是停留状态
-			if (p.isStop){
-				ctx.strokeStyle = color.toColor(0.2);
-				ctx.fillStyle = color.toColor(0.3);
-				ctx.arc(p.x, p.y, p.size * 4, 0, 2 * Math.PI);
-			}else{
-				ctx.strokeStyle = color.toColor(0.1);
-				ctx.fillStyle = color.toColor(0.15);
-				ctx.arc(p.x, p.y, p.size * 3, 0, 2 * Math.PI);
-			}
-			
-			ctx.fill();
-			ctx.stroke();
-		});
-		
-		// 画点
-		ctx.strokeStyle = color.toColor(0.5);
-		ctx.fillStyle = color.toColor(0.8);
-		point.forEach(function(p){
-			ctx.beginPath();
-			ctx.arc(p.x, p.y, p.size, 0, 2 * Math.PI);
-			ctx.fill();
-			ctx.stroke();
-		});
-		
-		distList.length = 0;
-		
-		requestAnimationFrame(draw);
-	}
+	}, 33);
 	
-};
+	
+	
+})();
